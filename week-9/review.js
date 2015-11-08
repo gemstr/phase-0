3e200a2550c60c41a8548c1141d4c98fcdfe5231
Joshua Kim <gemstr@gmail.com>
2015-11-07 18:52:30 -0800

    add review.js reflection

--- week-9/review.js
+++ week-9/review.js
@@ -1,7 +1,7 @@
 //Letter grade
-Pseudocode:
-Simple if and else if statements that look to return a letter grade
-example: above 90 is A, Below 89 but above or 80 equals B and so forth.
+// Pseudocode:
+// Simple if and else if statements that look to return a letter grade
+// example: above 90 is A, Below 89 but above or 80 equals B and so forth.
 
 
 function letter_grade(grade_num) {
@@ -19,3 +19,13 @@ function letter_grade(grade_num) {
 }
 
 console.log(letter_grade(90))
+
+//Reflection:
+// What concepts did you solidify in working on this challenge?
+// - I solidified creating if statements that include && in them.
+// What was the most difficult part of this challenge?
+// - I didn't think this challenge was particularly challenging, I just wanted to review by if and else if statements with && operators.
+// Did you solve the problem in a new way this time?
+// - I did not. I considered pull grade letters from an array, but I felt that would have been more code.
+// Was your pseudocode different from the Ruby version? What was the same and what was different?
+// - The approach was the same as the ruby version.
